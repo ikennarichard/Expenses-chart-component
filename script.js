a1716280fd1132971501b1d1b5053a3d4375a430
Ikenna Richard <oguejioforrichard@gmail.com>
2022-12-24 09:37:14 -1100

    Upadate script file

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 
+//bar charts
+let charts = [...document.querySelectorAll('.spending_ > span > span')];
+
+
 async function getData() {
-    try {
+
     const data = await fetch('./data.json');
 
     const res = await data.json();
+    console.log(res[0].day)
 
-    console.log(res);
-    }
+    charts.forEach((chart, i) => {
+        if (chart.id === res[i].day) 
+        chart.style.height = `${res[i].amount * 2}px`;
 
-    catch (err) {
-        console.log(err);
-    }
+        if(chart.id === 'wed') {
+            chart.style.backgroundColor = 'var(--cyan)'
+        }
+})
+} 
 
-    finally {
-
-    }
-
-
-}
+getData();
 
